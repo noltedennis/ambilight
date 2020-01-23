5d0de94726f88cb04bcceafb19d67a17e3c976c7
noltedennis <dennis_nolte@gmx.de>
2020-01-23 21:54:35 +0100

    Add sum file and external module

--- cmd/ambilight-server/main.go
+++ cmd/ambilight-server/main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/noltedennis/ambilight"
 	"github.com/noltedennis/ambilight/serverlib"
+	"github.com/stianeikeland/go-rpio/v4"
 )
 
 func main() {
 	fmt.Println(ambilight.Config())
 	fmt.Println(serverlib.Hello())
+
+	err := rpio.Open()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("RPIO device opened")
+	rpio.Close()
 }
